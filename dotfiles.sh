63eb8048a08767ef7fe41791655087997d0f0dee
tknawara <tarek.nawara@gmail.com>
2024-05-04 15:08:01 +0100

    Use home manager to manage my dotfiles.

--- dotfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -d "$HOME/.config/nvim" ]; then
-	mkdir -p "$HOME/.config/nvim"
-	echo "Created nvim folder successfully"
-fi
-
-stow -d dotfiles/.config nvim -t /home/tarek/.config/nvim
