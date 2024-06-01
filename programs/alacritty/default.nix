f989b66f4bf6d509b35e1e6aa2f0b6cb42bd79f4
tknawara <tarek.nawara@gmail.com>
2024-06-01 01:37:53 +0100

    Added dark borders for Alacritty.

--- programs/alacritty/default.nix
+++ programs/alacritty/default.nix
@@ -8,8 +8,11 @@
         normal = { family = "CaskaydiaMono Nerd Font"; };
         size = 11;
       };
+      env = { TERM = "xterm-256color"; };
       window = {
         decorations = "full";
+        dynamic_title = true;
+        decorations_theme_variant = "Dark";
         opacity = 0.9;
         padding = {
           x = 5;
