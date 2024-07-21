ed7dc84997373cc5dcf6cef15b8ac50f6f09b679
tknawara <tarek.nawara@gmail.com>
2024-07-21 20:15:52 +0100

    flake update.

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -4,6 +4,7 @@
   programs.tmux = {
     enable = true;
     escapeTime = 0;
+    clock24 = true;
     historyLimit = 10000;
     terminal = "screen-256color";
     plugins = with pkgs.tmuxPlugins; [ catppuccin ];
