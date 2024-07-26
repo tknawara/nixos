a997855459c9b12d93bd53761ad4333178792537
tknawara <tarek.nawara@gmail.com>
2024-07-26 23:30:55 +0100

    Remove zed editor.

--- home.nix
+++ home.nix
@@ -84,7 +84,6 @@
     cliphist
     yazi
     bibata-cursors
-    zed-editor
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
