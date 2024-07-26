ffc58d59c4efa2663d5f62c5712cf887ca4f64db
tknawara <tarek.nawara@gmail.com>
2024-07-26 19:16:56 +0100

    add zed editor.

--- home.nix
+++ home.nix
@@ -84,6 +84,7 @@
     cliphist
     yazi
     bibata-cursors
+    zed-editor
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
