074362bf667533e67cfdbac856cc39ec4c7e8f9c
tknawara <tarek.nawara@gmail.com>
2024-07-29 18:16:09 +0100

    Add zoxide support.

--- home.nix
+++ home.nix
@@ -19,6 +19,7 @@
     ./programs/hyprlock/default.nix
     ./programs/starship/default.nix
     ./programs/wezterm/default.nix
+    ./programs/zoxide/default.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
