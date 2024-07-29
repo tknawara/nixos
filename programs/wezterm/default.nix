dd7455e7659b37d128e02f4c13d21852d03f2b7a
tknawara <tarek.nawara@gmail.com>
2024-07-29 21:00:02 +0100

    Use wezterm flake.

--- programs/wezterm/default.nix
+++ programs/wezterm/default.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   programs.wezterm = {
     enable = true;
+    package = inputs.wezterm.packages.${pkgs.system}.default;
     enableZshIntegration = true;
     enableBashIntegration = true;
     extraConfig = builtins.readFile ./wezterm.lua;
