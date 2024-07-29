96fbf1de2ac3752df5ce32849209c9fad9b078cb
tknawara <tarek.nawara@gmail.com>
2024-07-29 18:05:40 +0100

    Add wezterm config

--- programs/fish/default.nix
+++ programs/fish/default.nix
@@ -6,6 +6,10 @@
     interactiveShellInit = ''
       set fish_greeting
       fish_vi_key_bindings
+      set fish_cursor_default block
+      set fish_cursor_insert line
+      set fish_cursor_replace_one underscore
+      set fish_cursor_visual block
     '';
     shellAliases = {
       ff = "fastfetch";
--- programs/wezterm/wezterm.lua
+++ programs/wezterm/wezterm.lua
@@ -6,5 +6,9 @@ config.color_scheme = 'Catppuccin Macchiato'
 config.window_decorations = 'NONE'
 config.font = wezterm.font 'CaskaydiaMono Nerd Font'
 config.harfbuzz_features = { 'calt', 'liga', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09' }
+config.enable_wayland = false
+config.enable_tab_bar = false
+config.window_background_opacity = 0.8
+config.default_cursor_style = 'SteadyBar'
 
 return config
