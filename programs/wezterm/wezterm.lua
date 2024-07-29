03f6a098ece47ef3dd6450bad8efc918c4201b9a
tknawara <tarek.nawara@gmail.com>
2024-07-29 18:20:25 +0100

    small tweak.

--- programs/wezterm/wezterm.lua
+++ programs/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ config.font = wezterm.font 'CaskaydiaMono Nerd Font'
 config.harfbuzz_features = { 'calt', 'liga', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09' }
 config.enable_wayland = false
 config.enable_tab_bar = false
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.default_cursor_style = 'SteadyBlock'
 
 return config
