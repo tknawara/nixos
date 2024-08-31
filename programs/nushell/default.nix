b9e1af7fe5eccdc093fe70dd0e9aca551ecd9f52
tknawara <tarek.nawara@gmail.com>
2024-09-01 00:22:25 +0100

    add nushell config file.

--- programs/nushell/default.nix
+++ programs/nushell/default.nix
@@ -3,6 +3,7 @@
 {
   programs.nushell = {
     enable = true;
+    configFile.source = ./config.nu;
     shellAliases = {
       ff = "fastfetch";
       update = "sudo nixos-rebuild switch --flake /home/tarek/nixos#default";
