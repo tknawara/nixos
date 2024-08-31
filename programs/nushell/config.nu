629ef10352ff760a40dccdfdd8d0ec3961043d5d
tknawara <tarek.nawara@gmail.com>
2024-09-01 00:51:29 +0100

    remove nushell vim indicators.

--- programs/nushell/config.nu
+++ programs/nushell/config.nu
@@ -1,3 +1,7 @@
+
+$env.PROMPT_INDICATOR = ""
+$env.PROMPT_INDICATOR_VI_INSERT = ""
+$env.PROMPT_INDICATOR_VI_NORMAL = ""
 $env.config = {
   show_banner: false
   edit_mode: vi
--- programs/nushell/default.nix
+++ programs/nushell/default.nix
@@ -6,6 +6,8 @@
     configFile.source = ./config.nu;
     shellAliases = {
       ff = "fastfetch";
+      cat = "bat";
+      grep = "rg";
       update = "sudo nixos-rebuild switch --flake /home/tarek/nixos#default";
     };
   };
