30c0968ae1a3e057d23fb27ff9238c2a8f63efc3
tknawara <tarek.nawara@gmail.com>
2024-11-25 00:20:59 +0000

    System update and disable for sublime.

--- programs/sublime/default.nix
+++ programs/sublime/default.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ sublime4 ];
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1w" # sublime4
-  ];
+  # home.packages = with pkgs; [ sublime4 ];
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "openssl-1.1.1w" # sublime4
+  # ];
 }
