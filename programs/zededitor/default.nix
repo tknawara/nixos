cb22aaeb05ee69f1f60d12614681aa235caa73b1
tknawara <tarek.nawara@gmail.com>
2024-12-28 23:30:54 +0000

    Tweaks to zed editor.

--- programs/zededitor/default.nix
+++ programs/zededitor/default.nix
@@ -10,7 +10,7 @@
       load_direnv = "direct";
       buffer_font_size = 16;
       buffer_font_weight = 500;
-      ui_font_size = 16;
+      ui_font_size = 17;
       vim = {
         cursor_blink = false;
         toggle_relative_line_numbers = true;
