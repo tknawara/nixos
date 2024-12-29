ce19b13360e56390a8362abda8c15eac5ef9c52d
tknawara <tarek.nawara@gmail.com>
2024-12-29 11:02:39 +0000

    Zed editor tweaks.

--- programs/zededitor/default.nix
+++ programs/zededitor/default.nix
@@ -8,10 +8,10 @@
       features = { copilot = false; };
       vim_mode = true;
       load_direnv = "direct";
-      buffer_font_size = 15;
+      buffer_font_size = 16;
+      ui_font_size = 16;
       buffer_font_weight = 500;
       buffer_font_family = "Cascadia Code NF";
-      ui_font_size = 16;
       vim = {
         cursor_blink = false;
         toggle_relative_line_numbers = true;
