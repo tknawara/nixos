0db15c46e0b9a8e6e6a2a8107fb9741b67a5c660
tknawara <tarek.nawara@gmail.com>
2024-12-31 11:31:22 +0000

    System update.

--- programs/zededitor/default.nix
+++ programs/zededitor/default.nix
@@ -3,14 +3,13 @@
 {
   programs.zed-editor = {
     enable = true;
-    extensions = [ "scala" "nix" "java" "assembly" ];
+    extensions = [ "scala" "nix" "java" "assembly" "log" ];
     userSettings = {
       features = { copilot = false; };
       vim_mode = true;
       load_direnv = "direct";
       buffer_font_size = 16;
       ui_font_size = 16;
-      buffer_font_weight = 500;
       buffer_font_family = "Cascadia Code NF";
       vim = {
         cursor_blink = false;
