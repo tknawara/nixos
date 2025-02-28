46a3f89c6857935e7354c3ea686bf6a0b938966e
tknawara <tarek.nawara@gmail.com>
2025-02-28 00:08:24 +0000

    Add font features to ghostty.

--- programs/ghostty/default.nix
+++ programs/ghostty/default.nix
@@ -4,6 +4,7 @@
     ''
       font-family = "${config.font.monospace.name}"
       font-size = 13
+      font-feature = "calt, liga, ss02, ss03, ss04, ss05, ss06, ss07, ss08, ss09,ss19, ss20"
       gtk-titlebar = false
       theme = "catppuccin-mocha"
       window-padding-x = 6
