c8b5ec0d1ad1e861e83ca6e7e4a93979aa90f2ba
tknawara <tarek.nawara@gmail.com>
2025-04-01 22:53:20 +0100

    hyprlock remove styling in favor of default theme.

--- programs/hyprlock/default.nix
+++ programs/hyprlock/default.nix
@@ -9,50 +9,50 @@
         hide_cursor = true;
       };
 
-      input-field = [{
-        size = {
-          width = 300;
-          height = 60;
-        };
-        outline_thickness = 4;
-        dots_size = 0.2;
-        dots_spacing = 0.2;
-        dots_center = true;
-        fade_on_empty = false;
-        placeholder_text = ''
-          <span foreground="##cdd6f4"><i>󰌾 Logged in as </i><span foreground="##cba6f7">$USER</span></span>'';
-        hide_input = false;
-        fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-        position = {
-          x = -0;
-          y = -35;
-        };
-        halign = "center";
-        valign = "center";
-      }];
+      # input-field = [{
+      #   size = {
+      #     width = 300;
+      #     height = 60;
+      #   };
+      #   outline_thickness = 4;
+      #   dots_size = 0.2;
+      #   dots_spacing = 0.2;
+      #   dots_center = true;
+      #   fade_on_empty = false;
+      #   placeholder_text = ''
+      #     <span foreground="##cdd6f4"><i>󰌾 Logged in as </i><span foreground="##cba6f7">$USER</span></span>'';
+      #   hide_input = false;
+      #   fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+      #   position = {
+      #     x = -0;
+      #     y = -35;
+      #   };
+      #   halign = "center";
+      #   valign = "center";
+      # }];
 
-      label = [
-        {
-          text = ''cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"'';
-          font_size = 25;
-          position = {
-            x = -30;
-            y = -150;
-          };
-          halign = "right";
-          valign = "top";
-        }
-        {
-          text = ''cmd[update:30000] echo "$(date +"%R")"'';
-          font_size = 90;
-          position = {
-            x = -30;
-            y = 0;
-          };
-          halign = "right";
-          valign = "top";
-        }
-      ];
+      # label = [
+      #   {
+      #     text = ''cmd[update:43200000] echo "$(date +"%A, %d %B %Y")"'';
+      #     font_size = 25;
+      #     position = {
+      #       x = -30;
+      #       y = -150;
+      #     };
+      #     halign = "right";
+      #     valign = "top";
+      #   }
+      #   {
+      #     text = ''cmd[update:30000] echo "$(date +"%R")"'';
+      #     font_size = 90;
+      #     position = {
+      #       x = -30;
+      #       y = 0;
+      #     };
+      #     halign = "right";
+      #     valign = "top";
+      #   }
+      # ];
 
       background = [{ path = "${config.wallpaper}"; }];
     };
