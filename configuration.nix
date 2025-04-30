c8f1948c472bae863e2da80314d696d6f137af31
tknawara <tarek.nawara@gmail.com>
2025-04-30 18:49:13 +0100

    System update.

--- configuration.nix
+++ configuration.nix
@@ -129,6 +129,11 @@
       #  wget
     ];
 
+  environment.variables = {
+    NIXOS_OZONE_WL = "1";
+    __GL_GSYNC_ALLOWED = "1";
+  };
+
   # fonts
   fonts = {
     packages = with pkgs; [
@@ -151,14 +156,12 @@
       };
     };
   };
-  
+
   # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
+  hardware.graphics = { enable = true; };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
 
@@ -184,7 +187,7 @@
     open = true;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
